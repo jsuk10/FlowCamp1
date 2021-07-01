932c0a21b4a38db2f86dcabd2a8a907b401210e0
khg0343 <sally0343@gmail.com>
2021-07-01 13:51:00 +0900

    Feat : Makes Tab

--- app/src/main/java/com/example/apps/MainActivity.java
+++ app/src/main/java/com/example/apps/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.apps;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import android.widget.TabHost;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TabHost tabHost1 = (TabHost) findViewById(R.id.tabHost1) ;
+        tabHost1.setup() ;
+
+        // 첫 번째 Tab. (탭 표시 텍스트:"TAB 1"), (페이지 뷰:"content1")
+        TabHost.TabSpec ts1 = tabHost1.newTabSpec("Tab Spec 1") ;
+        ts1.setContent(R.id.content1) ;
+        ts1.setIndicator("TAB 1") ;
+        tabHost1.addTab(ts1)  ;
+
+        // 두 번째 Tab. (탭 표시 텍스트:"TAB 2"), (페이지 뷰:"content2")
+        TabHost.TabSpec ts2 = tabHost1.newTabSpec("Tab Spec 2") ;
+        ts2.setContent(R.id.content2) ;
+        ts2.setIndicator("TAB 2") ;
+        tabHost1.addTab(ts2) ;
+
+        // 세 번째 Tab. (탭 표시 텍스트:"TAB 3"), (페이지 뷰:"content3")
+        TabHost.TabSpec ts3 = tabHost1.newTabSpec("Tab Spec 3") ;
+        ts3.setContent(R.id.content3) ;
+        ts3.setIndicator("TAB 3") ;
+        tabHost1.addTab(ts3) ;
+
     }
+
+
+
 }
--- app/src/main/java/com/example/apps/TabActivity1.java
+++
@@ -1,6 +0,0 @@
-package com.example.apps;
-
-public class TabActivity1 {
-
-
-}
