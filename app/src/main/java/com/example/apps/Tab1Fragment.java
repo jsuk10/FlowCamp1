dbdcaf84c915826ceacbca9a5abda492d8bca3f1
jsuk10 <jsuk970407@gmail.com>
2021-07-02 16:05:36 +0900

    feat : Add call function

--- app/src/main/java/com/example/apps/Tab1Fragment.java
+++ app/src/main/java/com/example/apps/Tab1Fragment.java
@@ -1,10 +1,12 @@
 package com.example.apps;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.ContentResolver;
-import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.view.LayoutInflater;
@@ -12,9 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -24,10 +24,9 @@ import java.util.ArrayList;
 
 public class Tab1Fragment extends Fragment {
     ListView listView;
-    ArrayList<String> arrayList;
-    ArrayAdapter arrayAdapter;
+    ArrayList<CustomContext> arrayLists;
+    ArrayAdapter arrayAdapters;
     View view;
-    private Context context;
 
     public Tab1Fragment() {
         // Required empty public constructor
@@ -43,23 +42,28 @@ public class Tab1Fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_tab1, container, false);
         listView = view.findViewById(R.id.tab1_listView); // xml의 listview
-        arrayList = new ArrayList<>(); // 빈 배열 목록.
-        arrayAdapter = new ArrayAdapter(getActivity(), android.R.layout.simple_expandable_list_item_1, arrayList);
-        listView.setAdapter(arrayAdapter);
-        listView.setAdapter(arrayAdapter);
+        arrayLists = new ArrayList<>(); // 빈 배열 목록.
+        arrayAdapters = new ArrayAdapter(getActivity(), android.R.layout.simple_expandable_list_item_1, arrayLists);
+        listView.setAdapter(arrayAdapters);
 
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
-                String selected_item = (String)adapterView.getItemAtPosition(position);
-                Toast.makeText(getActivity(), selected_item, Toast.LENGTH_SHORT).show();
+            public void onItemClick(AdapterView<?> adapterView, View views, int position, long id) {
+                CustomContext selected_item = (CustomContext)adapterView.getItemAtPosition(position);
+                Intent call = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:"+selected_item.getNumber()));
+                ((Activity)view.getContext()).startActivity(call);
             }
         });
-        Button button = (Button) view.findViewById(R.id.tab1_button);
-        button.setOnClickListener(v -> clickEvent());
+        view.findViewById(R.id.tab1_button).setOnClickListener(v -> reacContacts());
+        view.findViewById(R.id.tab1_button2).setOnClickListener(v -> addContacts());
         return view;
     }
-    public void clickEvent(){
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public void reacContacts(){
         if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
             // 사용자에게 권한을 요청합니다.
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.READ_CONTACTS}, 100);
@@ -69,10 +73,10 @@ public class Tab1Fragment extends Fragment {
         }
     }
 
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-
+    public void addContacts(){
+        
+        //리프레쉬 해줌
+        readContacts();
     }
 
     // 사용자가 대화 상자에서 ALLOW를 클릭하면이 메서드가 호출됩니다.
@@ -84,28 +88,27 @@ public class Tab1Fragment extends Fragment {
 
     //콘텐츠 리졸버를 사용하여 연락처를 읽는 함수
     private void readContacts() {
+        String name = "not";
+        String number = "dont Have";
+        arrayLists.clear();
         ContentResolver contentResolver = getActivity().getContentResolver();
         Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        String contact;
         if (cursor.moveToFirst()) {
             do {
-                contact = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))+ "\n";
+                name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME))+ "\n";
                 String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
                 Integer hasPhone = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
                 if (hasPhone > 0) {
                     Cursor cp = contentResolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                             ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
                     if (cp != null && cp.moveToFirst()) {
-                        contact += cp.getString(cp.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        number = cp.getString(cp.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                         cp.close();
                     }
-                }else{
-                    contact += "No Phone Number";
                 }
-                arrayList.add(contact);
-
+                arrayLists.add(new CustomContext(name,number));
             } while (cursor.moveToNext());
-            arrayAdapter.notifyDataSetChanged();
+            arrayAdapters.notifyDataSetChanged();
         }
     }
 }
